c343b72563e6d5a1736b3172cc355ba587f8204a
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-25 20:05:44 +0100

    install live

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Laravel Livewire</title>
+  <link rel="stylesheet" href="{{ asset('css/tailwind.css') }}">
+  @livewireStyles
+  @livewireScripts
+</head>
+<body class="flex flex-col h-screen justify-between">
+<header class="bg-blue-600 h-20">
+  <h1 class="text-4xl text-white ml-6">Livewire</h1>
+</header>
+<main class="flex-grow p-4 bg-blue-100">
+  @yield('content')
+</main>
+<footer class="bg-red-600 h-10">
+  <h1 class="text-center text-white">Footer</h1>
+</footer>
+
+</body>
+</html>
